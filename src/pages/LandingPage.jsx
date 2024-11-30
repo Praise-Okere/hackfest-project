5fbc53809eb7955503774a353612ae9add016e63
CinnamonfirJ <cinnamonfir@gmail.com>
2024-11-30 14:34:30 +0100

    minor fixes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../component/Navbar";
+import FeatureCard from "../sections/FeaturesCard";
 import Hero from "../sections/Hero";
 
 const LandingPage = () => {
@@ -6,6 +7,7 @@ const LandingPage = () => {
     <main>
       <Navbar />
       <Hero />
+      <FeatureCard />
     </main>
   );
 };
